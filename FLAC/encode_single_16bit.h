a7f0159eaf637fee6228422b9f88613fb92130e4
Arvindh <arvindh@tarkalabs.com>
2019-07-25 16:32:58 +0530

    Added flac framework

---
+++ FLAC/encode_single_16bit.h
@@ -0,0 +1,29 @@
+//
+//  encode_single_16bit.h
+//  flac
+//
+//  Created by Yifei Teng on 1/31/16.
+//  Copyright © 2016 sbooth.org. All rights reserved.
+//
+
+#ifndef encode_single_16bit_h
+#define encode_single_16bit_h
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <FLACiOS/all.h>
+
+typedef struct flacWriterState {
+    size_t length;
+    size_t capacity;
+    size_t pointer;
+    FLAC__byte *data;
+    bool success;
+} flacWriterState;
+
+void flacWriterStateDes(flacWriterState * state);
+
+flacWriterState * FLAC__encodeSingle16bit(int16_t *inputBuffer, unsigned int sample_rate, unsigned int total_samples);
+
+#endif /* encode_single_16bit_h */
